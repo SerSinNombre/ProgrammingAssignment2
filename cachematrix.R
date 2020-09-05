5309f3c13df80df8c0b2d5748bfb5fb67cddd0b5
unknown <sersinombre@gmail.com>
2020-09-05 14:08:32 -0500

    Quiz 2 completed according to the instructions. The modified file was cachematrix.R to write the functions makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function makes a special"matrix" object 
+## which cache its inverse
 
-## Write a short comment describing this function
+## The result is a list to:
+## 1. set the matrix value, 2. get the matrix value
+## 3. set the inverse matrix, 4. get the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The result of this function is the inverse of the special
+## "matrix" returned by 'makeCacheMatrix'. This function will obtain
+## the inverse from the cache if it has already been calculated
+## and the matrix has not changed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
